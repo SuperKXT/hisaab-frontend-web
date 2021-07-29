8ba7250efb1be5bb252b6642e4281d6347871834
Saad Khan <saadkhankxt@hotmail.com>
2021-07-29 11:06:19 +0500

    Add Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import Routes from './router'
+import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DayjsFnsUtils from '@date-io/dayjs';
+
+import GlobalStyles from 'helpers/GlobalStyles';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={(props) =>
+			localStorage.getItem("token") ? (
+				<Component {...props} />
+			) : (
+				<Redirect
+					to={{
+						pathname: "/login",
+						state: { from: props.location },
+					}}
+				/>
+			)
+		}
+	/>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<SnackbarProvider maxSnack={3}>
+				<MuiPickersUtilsProvider utils={DayjsFnsUtils}>
+					<Router>
+
+						<CssBaseline />
+						<GlobalStyles />
+
+						<ProtectedRoute
+							exact
+							path={"/"}
+							component={Home}
+						/>
+
+						<Route path="/login" exact component={SignIn} />
+
+						<Redirect
+							to={{
+								pathname: "/",
+							}}
+						/>
+
+					</Router>
+				</MuiPickersUtilsProvider>
+
+			</SnackbarProvider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
